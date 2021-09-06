7814dd99304e5a3beb4e4b373df9f6f2b22ca572
Ilya Nesterov <eildos95@gmail.com>
2021-09-06 18:49:51 +0300

    Add bio field to User model, views, templates

--- everycheese/users/models.py
+++ everycheese/users/models.py
@@ -5,12 +5,12 @@ from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
-
     # First Name and Last Name Do Not Cover Name Patterns
     # Around the Globe.
     name = models.CharField(
         _("Name of User"), blank=True, max_length=255
     )
+    bio = models.TextField("Bio", blank=True)
 
     def get_absolute_url(self):
         return reverse(
