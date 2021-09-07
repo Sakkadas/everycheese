df2a028fe68dfbb73636ec10749ec1a9bfafa4c6
Ilya Nesterov <eildos95@gmail.com>
2021-09-07 19:26:37 +0300

    First pass at working Add Cheese form

--- everycheese/cheeses/models.py
+++ everycheese/cheeses/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from autoslug import AutoSlugField
 from model_utils.models import TimeStampedModel
 from django_countries.fields import CountryField
+from django.urls import reverse
 
 
 class Cheese(TimeStampedModel):
@@ -19,6 +20,10 @@ class Cheese(TimeStampedModel):
     firmness = models.CharField("Firmness", max_length=20,
                                 choices=Firmness.choices, default=Firmness.UNSPECIFIED)
     country_of_origin = CountryField("Country of Origin", blank=True)
+
+    def get_absolute_url(self):
+        return reverse('cheeses:detail',
+                       kwargs={"slug": self.slug})
 
     def __str__(self):
         return self.name
